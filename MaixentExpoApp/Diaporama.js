7a0390f73f3d4fc8a2f7eecfbeefc73fce168d5b
pbillerot <philippe.billerot@gmail.com>
2020-02-05 17:47:05 +0100

    diapo_publipostage paramétré dans une feuille

--- MaixentExpoApp/Diaporama.js
+++ MaixentExpoApp/Diaporama.js
@@ -280,3 +280,137 @@ function diapo_publipostageBadge24(sheetId, sheetName, filterName, filterValue) 
   
   return oDiaporamaCible.getId();
 }
+
+/**
+ * diapo_publipostage : Scripts de publipostage
+ * Les parmètres du publipostage sont dans une feuille à partir de la cellule B2
+ * Fichier des données     : l'url du tableur des données
+ * Feuille des données     : la feuille qui contient les données
+ * Ligne des données       : à partir de cette ligne
+ * Colonne à filtrer       : colonne qui sert au filtrage éventuel
+ * Valeur du filtre        : expression régulière du filtre
+ * Répertoire du résultat  : répertoire du fichier résultat
+ * Nom du fichier résultat : nom du fichier résultat
+ * Convertir en Pdf        : fichier résultat qui sera convertit en pdf
+ * Lettre                  : url du 1er fichier à inclure dans le résultat
+ * Annexe 1                : url de l'annexe 1
+ * Annexe 2                : etc
+ * Annexe 3                : etc
+ * Annexe 4                : etc
+ */
+function diapo_publipostage() {
+  var ui = SpreadsheetApp.getUi(); //
+  var yesnoConfirm = ui.alert(
+    "PUBLIPOSTAGE",
+    'Veuillez confirmer par oui ou non',
+    ui.ButtonSet.YES_NO);
+  if (yesnoConfirm != ui.Button.YES) return;
+  
+  var gabaritSheet = SpreadsheetApp.getActiveSheet()
+  // lecture des paramètres
+  var sheetUrl = gabaritSheet.getRange("B2").getValue()
+  var sheetName = gabaritSheet.getRange("B3").getValue()
+  var firstLineData = gabaritSheet.getRange("B4").getValue()
+  var filterName = gabaritSheet.getRange("B5").getValue()
+  var filterValue = gabaritSheet.getRange("B6").getValue()
+  var folderUrl = gabaritSheet.getRange("B7").getValue()
+  var slideName = gabaritSheet.getRange("B8").getValue()
+  var inPdf = gabaritSheet.getRange("B9").getValue()
+  var docs = []
+  docs.push(gabaritSheet.getRange("B10").getValue())
+  docs.push(gabaritSheet.getRange("B11").getValue())
+  docs.push(gabaritSheet.getRange("B12").getValue())
+  docs.push(gabaritSheet.getRange("B13").getValue())
+  docs.push(gabaritSheet.getRange("B14").getValue())
+
+  // Chargement des données
+  var spreadsheet = SpreadsheetApp.openByUrl(sheetUrl)
+  var sheet = spreadsheet.getSheetByName(sheetName)
+  var iLastCol = sheet.getLastColumn()
+  var iLastRow = sheet.getLastRow()
+  // chargement global de toutes les données de la feuille
+  // pour optimiser les ressources du serveur de Google
+  var sValues = sheet.getRange(1, 1, iLastRow, iLastCol).getValues()
+  // Lecture de la ligne d'entête pour mémoriser le nom des colonnes et leur position
+  var iCols = {}
+  var sCell = ""
+  var iRow = 0
+  var iCol
+  for (iCol = 0; iCol < iLastCol; iCol++) {
+    sCell = ("" + sValues[iRow][iCol]).trim()
+    if (sCell != "") {
+      iCols[sCell] = iCol
+    } // endif
+  } // endfor
+  // On ne prend que les lignes qui correspondent au critère filterName filterValue
+  var sDatas = []
+  iLastRow = sValues.length
+  var reFilter = new RegExp(filterValue, 'g')
+  for (iRow = parseInt(firstLineData)-1; iRow < iLastRow; iRow++) {
+    if (("" + sValues[iRow][iCols[filterName]]).match(reFilter, 'g') != null) {
+      sDatas.push(sValues[iRow])
+    } // endif
+  } // endfor
+  iLastRow = sDatas.length
+  
+  // Récupération de la lettre et des annexes
+  var slidesAll = []
+  var diaporamaModele = null // le 1er document servira de modèle au diaporama cible
+  for ( var idoc in docs ) {
+    if ( docs[idoc] != "" ) {
+      var presentation = SlidesApp.openByUrl(docs[idoc])
+      if ( diaporamaModele == null ) {
+        diaporamaModele = presentation
+      }
+      var slides = presentation.getSlides()
+      for (var islide=0; islide<slides.length; islide++) {
+        slidesAll.push(slides[islide])
+      }  
+    }
+  }
+
+  // Duplication du diaporama modèle
+  var fileModele = DriveApp.getFileById(diaporamaModele.getId())
+  const regex = /.*\/folders\/(.*)/g
+  var folderId = regex.exec(folderUrl)[1]
+  var folderCopy = DriveApp.getFolderById(folderId)
+  var fileCopy = fileModele.makeCopy(slideName, folderCopy)
+  var diaporamaCible = SlidesApp.openById(fileCopy.getId())
+  // suppression des slides
+  var slides = diaporamaCible.getSlides()
+  for (var i in slides) {
+    slides[i].remove()
+  }
+
+  // duplication des diapos autant de fois que d'enregistrements
+  for (iRow = 0; iRow < iLastRow; iRow++) {
+    for (var islide=0; islide<slidesAll.length; islide++) {
+      diaporamaCible.appendSlide(slidesAll[islide])
+    }
+  } // endfor
+  
+  var diapoCibles = diaporamaCible.getSlides()
+  
+  // OK, maintenant on fusionne les données dans les diapos
+  var iDiapo = 0
+  for (iRow = 0; iRow < iLastRow; iRow++) {
+    for (var islide=0; islide<slidesAll.length; islide++) {
+      // Recherche des colonnes dans le document et fusion des données
+      for (var key in iCols) {
+        sCell = ("" + sDatas[iRow][iCols[key]]).trim()
+        diapoCibles[iDiapo].replaceAllText("{$date}", fx_frenchDate(new Date()));
+        diapoCibles[iDiapo].replaceAllText("{" + key + "}", sCell)
+      } // endfor key
+      iDiapo++
+    }
+  } // endfor tableur
+  diaporamaCible.saveAndClose();
+
+  // affichage d'un panneau pour ouvrir le document crée
+  var url = diaporamaCible.getUrl()
+  var htmlOutput = HtmlService
+    .createHtmlOutput('<a href="' + url + '" target="_blank">Voir le résultat</a>')
+    .setWidth(300)
+    .setHeight(100)
+  SpreadsheetApp.getUi().showModalDialog(htmlOutput, "Script terminé")
+}
