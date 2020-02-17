38827d2dfb901a66d7c5f9a6f8bc565a78311ae1
pbillerot <philippe.billerot@gmail.com>
2020-02-17 17:33:24 +0100

    fusion plan

--- MaixentExpoApp/Stands.js
+++ MaixentExpoApp/Stands.js
@@ -1,30 +1,17 @@
 /**
- * PublipostageBadge24 est en fait un publipostage de vignettes
- * Les parmètres du publipostage sont dans une feuille à partir de la cellule B2
- * Fichier des données     : l'url du tableur des données
- * Feuille des données     : la feuille qui contient les données
- * Ligne des données       : à partir de cette ligne
- * Colonne à filtrer       : colonne qui sert au filtrage éventuel
- * Valeur du filtre        : expression régulière du filtre
- * Répertoire du résultat  : répertoire du fichier résultat
- * Nom du fichier résultat : nom du fichier résultat
- * Convertir en Pdf        : option pour convertir en pdf le résultat
- * Lettre                  : url du 1er fichier à inclure dans le résultat
- * Annexe 1                : url de l'annexe 1
- * Annexe 2                : etc
- * Annexe 3                : etc
- * Annexe 4                : etc
+ * stands_grand_hall_fusionEmplacements réalise la fusion de données dans le slide
+ * 
  */
-function stands_grand_hall_fusionEmplacements() {
+function stands_fusionPlan() {
   var ui = SpreadsheetApp.getUi(); //
   var yesnoConfirm = ui.alert(
-    "PUBLIPOSTAGE",
+    "FUSION DES DONNEES",
     'Veuillez confirmer par Oui ou Non',
     ui.ButtonSet.YES_NO);
   if (yesnoConfirm != ui.Button.YES) return;
-  
-  // lecture des paramètres
+
   var piloteSheet = SpreadsheetApp.getActiveSheet()
+  // lecture des paramètres
   var sheetUrl = piloteSheet.getRange("B2").getValue()
   var sheetName = piloteSheet.getRange("B3").getValue()
   var firstLineData = piloteSheet.getRange("B4").getValue()
@@ -33,12 +20,7 @@ function stands_grand_hall_fusionEmplacements() {
   var folderUrl = piloteSheet.getRange("B7").getValue()
   var slideName = piloteSheet.getRange("B8").getValue()
   var inPdf = piloteSheet.getRange("B9").getValue()
-  var docs = []
-  docs.push(piloteSheet.getRange("B10").getValue())
-  docs.push(piloteSheet.getRange("B11").getValue())
-  docs.push(piloteSheet.getRange("B12").getValue())
-  docs.push(piloteSheet.getRange("B13").getValue())
-  docs.push(piloteSheet.getRange("B14").getValue())
+  var modele = piloteSheet.getRange("B10").getValue()
 
   // Ouverture de la feuille
   var spreadsheet = SpreadsheetApp.openByUrl(sheetUrl)
@@ -47,48 +29,36 @@ function stands_grand_hall_fusionEmplacements() {
   var iLastRow = sheet.getLastRow()
   // chargement global de toutes les données de la feuille
   // pour optimiser les ressources du serveur de Google
-  var sValues = sheet.getRange(1, 1, iLastRow, iLastCol).getValues()
+  var sValues = sheet.getRange(1, 1, iLastRow, iLastCol).getValues();
   // Lecture de la ligne d'entête pour mémoriser le nom des colonnes et leur position
-  var iCols = {}
-  var sCell = ""
-  var iRow = 0
-  var iCol
-  for (iCol = 0; iCol < iLastCol; iCol++) {
-    sCell = ("" + sValues[iRow][iCol]).trim()
-    if (sCell != "") {
-      iCols[sCell] = iCol
+  var iCols = {};
+  var sCell = "";
+  var iRow = 0;
+  var iCol;
+  for(iCol=0; iCol<iLastCol; iCol++) {
+    sCell = ("" + sValues[iRow][iCol]).trim();
+    if ( sCell != "" ) {
+      iCols[sCell] = iCol;
     } // endif
   } // endfor
+
   // On ne prend que les lignes qui correspondent au critère filterName filterValue
   var sDatas = []
   iLastRow = sValues.length
   var reFilter = new RegExp(filterValue, 'g')
-  for (iRow = parseInt(firstLineData)-1; iRow < iLastRow; iRow++) {
+  for (iRow = parseInt(firstLineData) - 1; iRow < iLastRow; iRow++) {
     if (("" + sValues[iRow][iCols[filterName]]).match(reFilter, 'g') != null) {
       sDatas.push(sValues[iRow])
     } // endif
   } // endfor
   iLastRow = sDatas.length
-  if ( iLastRow == 0 ) {
-    ui.alert("PUBLIPOSTAGE", "Aucun enregistrement trouvé", ui.ButtonSet.OK)
+  if (iLastRow == 0) {
+    ui.alert("FUSION", "Aucun enregistrement trouvé", ui.ButtonSet.OK)
     return
   }
-  // Récupération de la lettre et des annexes
-  var slidesAll = []
-  var diaporamaModele = null // le 1er document servira de modèle au diaporama cible
-  for ( var idoc in docs ) {
-    if ( docs[idoc] != "" ) {
-      var presentation = SlidesApp.openByUrl(docs[idoc])
-      if ( diaporamaModele == null ) {
-        diaporamaModele = presentation
-      }
-      var slides = presentation.getSlides()
-      for (var islide=0; islide<slides.length; islide++) {
-        slidesAll.push(slides[islide])
-      }  
-    }
-  }
 
+  // Récupération de la diapo Modèle
+  var diaporamaModele = SlidesApp.openByUrl(modele)
   // Duplication du diaporama modèle
   var fileModele = DriveApp.getFileById(diaporamaModele.getId())
   const regex = /.*\/folders\/(.*)/g
@@ -96,13 +66,8 @@ function stands_grand_hall_fusionEmplacements() {
   var folderCopy = DriveApp.getFolderById(folderId)
   var fileCopy = fileModele.makeCopy(slideName, folderCopy)
   var diaporamaCible = SlidesApp.openById(fileCopy.getId())
-  // suppression des slides
-  // var slides = diaporamaCible.getSlides()
-  // for (var i in slides) {
-  //   slides[i].remove()
-  // }
   var diapoCibles = diaporamaCible.getSlides()
-
+  
   // OK, maintenant on fusionne les données dans la diapo cible
   var sKey = "";
   var sIndexValue;
@@ -110,7 +75,7 @@ function stands_grand_hall_fusionEmplacements() {
   for(iRow=0; iRow<iLastRow; iRow++) {
     // Parcours des colonnes du tableur et fusion des données dans le slide
     for( var key in iCols) {
-      sIndexValue = ("" + sDatas[iRow][iCols[indexName]]).trim();
+      sIndexValue = ("" + sDatas[iRow][iCols[filterName]]).trim(); // filterName sert d'index
       sKey = "{" + key + "_" + sIndexValue + "}"; 
       sCell = ("" + sDatas[iRow][iCols[key]]).trim();
       diapoCibles[0].replaceAllText(sKey, sCell);
@@ -153,7 +118,7 @@ function stands_grand_hall_fusionEmplacements() {
   diaporamaCible.saveAndClose();
 
   var url = ""
-  if ( inPdf ) {
+  if (inPdf) {
     var blob = DriveApp.getFileById(fileCopy.getId()).getBlob()
     var pdfFile = DriveApp.createFile(blob)
     // le fichier a été crée dans la racine du répertoire de l'utilisateur
@@ -161,7 +126,7 @@ function stands_grand_hall_fusionEmplacements() {
     DriveApp.getFolderById(folderId).addFile(pdfFile); // ajout du répertoire cible
     DriveApp.getRootFolder().removeFile(pdfFile); // suppresion du répertoire racine du fichier
     var url = pdfFile.getUrl()
-    fileCopy.setTrashed(true)  
+    fileCopy.setTrashed(true)
   } else {
     var url = fileCopy.getUrl()
   }
